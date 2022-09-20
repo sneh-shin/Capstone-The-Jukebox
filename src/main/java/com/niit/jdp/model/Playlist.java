3da5f96b494e365c5cfbd84be8022b526ec0f858
Sneha <snehasoftwareengineering123@gmail.com>
2022-09-20 23:04:23 +0530

    added getters and setters for private fields

--- src/main/java/com/niit/jdp/model/Playlist.java
+++ src/main/java/com/niit/jdp/model/Playlist.java
@@ -20,4 +20,28 @@ public class Playlist {
         this.playlistName = playlistName;
         this.songList = songList;
     }
+
+    public int getPlaylistId() {
+        return playlistId;
+    }
+
+    public void setPlaylistId(int playlistId) {
+        this.playlistId = playlistId;
+    }
+
+    public String getPlaylistName() {
+        return playlistName;
+    }
+
+    public void setPlaylistName(String playlistName) {
+        this.playlistName = playlistName;
+    }
+
+    public List<Song> getSongList() {
+        return songList;
+    }
+
+    public void setSongList(List<Song> songList) {
+        this.songList = songList;
+    }
 }
