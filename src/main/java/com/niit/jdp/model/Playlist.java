31f33027a843b9d3c9564468a04751ec70ccefe0
Sneha <snehasoftwareengineering123@gmail.com>
2022-09-20 23:03:50 +0530

    added no-args and parameterised constructor in the Playlist.java

--- src/main/java/com/niit/jdp/model/Playlist.java
+++ src/main/java/com/niit/jdp/model/Playlist.java
@@ -11,4 +11,13 @@ public class Playlist {
     private int playlistId;
     private String playlistName;
     private List<Song> songList;
+
+    public Playlist() {
+    }
+
+    public Playlist(int playlistId, String playlistName, List<Song> songList) {
+        this.playlistId = playlistId;
+        this.playlistName = playlistName;
+        this.songList = songList;
+    }
 }
