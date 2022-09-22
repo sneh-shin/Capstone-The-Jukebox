071f61c747a0b7b8cd53233ed7afc70c02e0539a
Sneha <snehasoftwareengineering123@gmail.com>
2022-09-22 15:24:38 +0530

    created the object for MusicPlayerService.java class

--- src/test/java/com/niit/jdp/service/MusicPlayerServiceTest.java
+++ src/test/java/com/niit/jdp/service/MusicPlayerServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MusicPlayerServiceTest {
+    MusicPlayerService musicPlayerService;
 
     @BeforeEach
     void setUp() {
