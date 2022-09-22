edcd82cc89ea976a492317c953be51bfc8fd2856
Sneha <snehasoftwareengineering123@gmail.com>
2022-09-22 15:16:31 +0530

    added connectSuccess() method

--- src/test/java/com/niit/jdp/service/DatabaseServiceTest.java
+++ src/test/java/com/niit/jdp/service/DatabaseServiceTest.java
@@ -1,14 +1,21 @@
 package com.niit.jdp.service;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
+
 class DatabaseServiceTest {
     DatabaseService databaseService;
+    private static final String URL = "jdbc:mysql://localhost:3306/jukebox";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
 
     @BeforeEach
     void setUp() {
+        databaseService = new DatabaseService();
     }
 
     @AfterEach
@@ -16,6 +23,16 @@ class DatabaseServiceTest {
     }
 
     @Test
-    void connect() {
+    void connectSuccess() throws SQLException, ClassNotFoundException {
+        boolean expected = true;
+        boolean actual = databaseService.connect();
+        Assertions.assertEquals(true, actual);
+    }
+
+    @Test
+    void connectFailure() throws SQLException, ClassNotFoundException {
+        boolean expected = false;
+        boolean actual = databaseService.connect();
+        Assertions.assertNotEquals(expected, actual);
     }
 }
