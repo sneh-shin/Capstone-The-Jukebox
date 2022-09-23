b0d2576d970cbc3c8e8edfaf5494ce86c3e9cbd6
Sneha <snehasoftwareengineering123@gmail.com>
2022-09-23 11:50:53 +0530

    added getByIdSuccess() and getByIdFailure() test cases

--- src/test/java/com/niit/jdp/repository/PlaylistRepositoryTest.java
+++ src/test/java/com/niit/jdp/repository/PlaylistRepositoryTest.java
@@ -1,14 +1,25 @@
 package com.niit.jdp.repository;
 
+import com.niit.jdp.model.Playlist;
+import com.niit.jdp.service.DatabaseService;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
+
 class PlaylistRepositoryTest {
+    DatabaseService databaseService;
     PlaylistRepository playlistRepository;
+    Playlist playlist;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws SQLException, ClassNotFoundException {
+        databaseService = new DatabaseService();
+        playlistRepository = new PlaylistRepository();
+        playlist = new Playlist();
+        databaseService.connect();
     }
 
     @AfterEach
@@ -16,6 +27,14 @@ class PlaylistRepositoryTest {
     }
 
     @Test
-    void getById() {
+    void getByIdSuccess() throws SQLException {
+        playlist = playlistRepository.getById(databaseService.getConnection(), 1);
+        Assertions.assertEquals("vibesss", playlist.getPlaylistName());
+    }
+
+    @Test
+    void getByIdFailure() throws SQLException {
+        playlist = playlistRepository.getById(databaseService.getConnection(), 1);
+        Assertions.assertNotEquals(null, playlist.getPlaylistName());
     }
 }
