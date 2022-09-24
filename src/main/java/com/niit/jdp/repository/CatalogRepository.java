213a14c0dd644d8b847f663ae3c5f7f3bfdc12c1
Sneha <snehasoftwareengineering123@gmail.com>
2022-09-24 20:12:49 +0530

    added displayHeader() method

--- src/main/java/com/niit/jdp/repository/CatalogRepository.java
+++ src/main/java/com/niit/jdp/repository/CatalogRepository.java
@@ -220,6 +220,8 @@ public class CatalogRepository {
             System.out.println("Press any key to go back to menu");
         } else if (songChoice == 1) {
             musicPlayerService.pause();
+        } else {
+            System.err.println("Invalid Choice!!");
         }
     }
 }
